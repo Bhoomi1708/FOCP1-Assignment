66f11fdec0f01e69003409b80f02b085e3210a9b
Bhoomi Aggarwal <bhoomiaggarwal1708@gmail.com>
2025-11-07 21:25:19 +0530

    Program to check Armstrong number

--- Assignment_1/Q1.c
+++ Assignment_1/Q1.c
@@ -6,31 +6,27 @@ int main() {
     int num, rem, count = 0;
     double arm = 0;  // can use double for safety with pow()
     
-    printf("\nEnter number to be checked for Armstrong Number: ");
+    printf("\nEnter the number : ");
     scanf("%d", &num);
 
-    int copy = num;  // store original number
-
-    // Step 1: Count digits
+    int copy = num;  
     int temp = num;
     while (temp > 0) {
         count++;
         temp = temp / 10;
     }
 
-    // Step 2: Calculate Armstrong sum
     temp = num;
     while (temp > 0) {
         rem = temp % 10;
-        arm = arm + ceil(pow(rem, count));  // using ceil to avoid float rounding issue
+        arm = arm + ceil(pow(rem, count));  
         temp = temp / 10;
     }
 
-    // Step 3: Compare and print
     if ((int)arm == copy)
-        printf("\nIt's an Armstrong Number.\n");
+        printf("\nIt is an Armstrong Number.\n");
     else
-        printf("\nIt's not an Armstrong Number.\n");
+        printf("\nIt is not an Armstrong Number.\n");
 
     return 0;
 }
