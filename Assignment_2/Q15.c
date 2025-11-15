7eae84fb895a45c73b9b72fd96b1a8c84a20d6a9
Bhoomi Aggarwal <bhoomiaggarwal1708@gmail.com>
2025-11-16 01:31:26 +0530

    pattern printing program
    Implement a program to print a pattern of 0s and 1s.

---
+++ Assignment_2/Q15.c
@@ -0,0 +1,32 @@
+//Ques15
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int num;
+    int spaces = 12;  
+
+    for (i = 1; i <= 5; i++) {
+        num = 0;
+
+    for (j = 1; j <= i; j++) {
+            printf("%d", num);
+            num = 1 - num;
+        }
+      
+    for (j = 1; j <= spaces; j++) {
+            printf(" ");
+        }
+
+        num = 0;
+
+        for (j = 1; j <= i; j++) {
+            printf("%d", num);
+            num = 1 - num;
+        }
+
+        printf("\n");
+spaces -= 4;  
+    }
+    return 0;
+}
